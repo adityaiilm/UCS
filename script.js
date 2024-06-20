85ed0d51ea336b6fc52a70e148d4b992d911fb17
sagar <sagarshigvan28@gmail.com>
2024-06-20 13:59:34 +0530

    hmm

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let SignUp_button = document.querySelector("#signbtn");
+let Cancel_btn = document.querySelector("#cross");
 
 let Profile_button = document.querySelector("#profile");
 let Menu =  document.querySelector(".dropdown");
 
 //signup button
 SignUp_button.addEventListener("click", () => {
-    alert("button clicked");
+    // alert("button clicked");
     document.querySelector(".OverlaySheet").classList.add("showoverlay");
     document.querySelector(".signup-page").classList.add("showsignup-page");
 })
+
+Cancel_btn.addEventListener("click", () => {
+    document.querySelector(".OverlaySheet").classList.remove("showoverlay");
+    document.querySelector(".signup-page").classList.remove("showsignup-page");
+})
+
 
 //profile button dropdown
 Profile_button.addEventListener("click", () => {
