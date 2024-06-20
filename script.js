cd33a58f93defb1716856fa2e880b48e757bbbec
sagar <sagarshigvan28@gmail.com>
2024-06-20 23:47:47 +0530

    basic changes are done

--- script.js
+++ script.js
@@ -9,13 +9,26 @@ SignUp_button.addEventListener("click", () => {
     // alert("button clicked");
     document.querySelector(".OverlaySheet").classList.add("showoverlay");
     document.querySelector(".signup-page").classList.add("showsignup-page");
-})
+});
 
 Cancel_btn.addEventListener("click", () => {
     document.querySelector(".OverlaySheet").classList.remove("showoverlay");
     document.querySelector(".signup-page").classList.remove("showsignup-page");
-})
+});
 
+
+let C_individual = document.querySelector("#tbx1");
+let C_organisation = document.querySelector("#tbx2");
+//choose identity
+C_individual.addEventListener("click", () => {
+    alert("button clicked");
+   
+});
+
+C_organisation.addEventListener("click", () => {
+    alert("button clicked");
+    
+});
 
 //profile button dropdown
 Profile_button.addEventListener("click", () => {
