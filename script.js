ad39e349b39e43b62bf4e5fe46aa2cc2b7a25218
sagar <sagarshigvan28@gmail.com>
2024-07-04 13:30:17 +0530

    some changes are done

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
-let SignUp_button = document.querySelector("#signbtn");
-let Cancel_btn = document.querySelector("#cross");
+let switch = document.getElementById("switch")
 
-let Profile_button = document.querySelector("#profile");
-let Menu =  document.querySelector(".dropdown");
+function leftClick(){
+    switch.style.left = 0;
+}
 
-//signup button
-SignUp_button.addEventListener("click", () => {
-    // alert("button clicked");
-    document.querySelector(".OverlaySheet").classList.add("showoverlay");
-    document.querySelector(".signup-page").classList.add("showsignup-page");
-});
-
-Cancel_btn.addEventListener("click", () => {
-    document.querySelector(".OverlaySheet").classList.remove("showoverlay");
-    document.querySelector(".signup-page").classList.remove("showsignup-page");
-});
+function rightClick(){
+    switch.style.left = 5vw;
+}
 
 
-let C_individual = document.querySelector("#tbx1");
-let C_organisation = document.querySelector("#tbx2");
-//choose identity
-C_individual.addEventListener("click", () => {
-    alert("button clicked");
+// let SignUp_button = document.querySelectorAll("#signbtn");
+// let Cancel_btn = document.querySelector("#cross");
+
+// //signup button
+// SignUp_button.addEventListener("click", () => {
+//     // alert("button clicked");
+//     document.querySelector(".OverlaySheet").classList.add("showoverlay");
+//     document.querySelector(".signup-page").classList.add("showsignup-page");
+// });
+
+// Cancel_btn.addEventListener("click", () => {
+//     document.querySelector(".OverlaySheet").classList.remove("showoverlay");
+//     document.querySelector(".signup-page").classList.remove("showsignup-page");
+// });
+
+// let Profile_button = document.querySelector("#profile");
+// let Menu =  document.querySelector(".dropdown");
+
+// //profile button dropdown
+// Profile_button.addEventListener("click", () => {
+//     Menu.classList.toggle("dropdownShow");
+// });
+
+// Profile_button.addEventListener("click", () => {
+//     if( Menu.classList.contains("dropdownShow")){
+//        Menu.classList.removes("dropdownShow")
+//     }
+// });
+
+// let C_individual = document.querySelector("#tbx1");
+// let C_organisation = document.querySelector("#tbx2");
+// //choose identity
+// C_individual.addEventListener("click", () => {
+//     alert("button clicked");
    
-});
+// });
 
-C_organisation.addEventListener("click", () => {
-    alert("button clicked");
+// C_organisation.addEventListener("click", () => {
+//     alert("button clicked");
     
-});
+// });
 
-//profile button dropdown
-Profile_button.addEventListener("click", () => {
-    Menu.classList.toggle("dropdownShow");
-});
 
-Profile_button.addEventListener("click", () => {
-    if( Menu.classList.contains("dropdownShow")){
-       Menu.classList.removes("dropdownShow")
-    }
-});
