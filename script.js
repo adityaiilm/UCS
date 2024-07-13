fb21b6543480268dac38b0419d1952013834ca72
sagar <sagarshigvan28@gmail.com>
2024-07-13 14:30:32 +0530

    few updates

--- script.js
+++ script.js
@@ -1,54 +1,78 @@
-let switch = document.getElementById("switch")
+let LoaderPage = document.querySelector("#loader")
+
+setTimeout(function() {
+    LoaderPage.style.top = "-100%"
+},3000)
+
+
+function signupPagePopUP(){
+    
+let SignUp_button = document.querySelector("#signbtn");
+let Cancel_btn = document.querySelector("#cross");
+
+//signup button
+SignUp_button.addEventListener("click", () => {
+    // alert("button clicked");
+    document.querySelector(".OverlaySheet").classList.add("showoverlay");
+    document.querySelector(".signup-page").classList.add("showsignup-page");
+});
+
+Cancel_btn.addEventListener("click", () => {
+    document.querySelector(".OverlaySheet").classList.remove("showoverlay");
+    document.querySelector(".signup-page").classList.remove("showsignup-page");
+});
+}
+function profileDropdown(){
+    let Profile_button = document.querySelector("#profile");
+    let Menu =  document.querySelector(".dropdown");
+
+//profile button dropdown
+Profile_button.addEventListener("click", () => {
+    Menu.classList.toggle("dropdownShow");
+});
+
+Profile_button.addEventListener("click", () => {
+    if( Menu.classList.contains("dropdownShow")){
+        Menu.classList.removes("dropdownShow")
+    }
+});
+}
+
+function chooseIdentity(){
+    let C_individual = document.querySelector("#tbx1");
+let C_organisation = document.querySelector("#tbx2");
+//choose identity
+C_individual.addEventListener("click", () => {
+    alert("button clicked");
+   
+});
+
+C_organisation.addEventListener("click", () => {
+    alert("button clicked");
+    
+});
+}
+var switchbtn = document.getElementById("switch");
 
 function leftClick(){
-    switch.style.left = 0;
+    switchbtn.style.left = 0;
 }
-
 function rightClick(){
-    switch.style.left = 5vw;
+    switchbtn.style.left = "150px";
+    switchbtn.style.color = "white";
 }
 
+function importData() {
+    let input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = _ => {
+      // you can use this method to get file and perform respective operations
+              let files =   Array.from(input.files);
+              console.log(files);
+          };
+    input.click();
+}
 
-// let SignUp_button = document.querySelectorAll("#signbtn");
-// let Cancel_btn = document.querySelector("#cross");
-
-// //signup button
-// SignUp_button.addEventListener("click", () => {
-//     // alert("button clicked");
-//     document.querySelector(".OverlaySheet").classList.add("showoverlay");
-//     document.querySelector(".signup-page").classList.add("showsignup-page");
-// });
-
-// Cancel_btn.addEventListener("click", () => {
-//     document.querySelector(".OverlaySheet").classList.remove("showoverlay");
-//     document.querySelector(".signup-page").classList.remove("showsignup-page");
-// });
-
-// let Profile_button = document.querySelector("#profile");
-// let Menu =  document.querySelector(".dropdown");
-
-// //profile button dropdown
-// Profile_button.addEventListener("click", () => {
-//     Menu.classList.toggle("dropdownShow");
-// });
-
-// Profile_button.addEventListener("click", () => {
-//     if( Menu.classList.contains("dropdownShow")){
-//        Menu.classList.removes("dropdownShow")
-//     }
-// });
-
-// let C_individual = document.querySelector("#tbx1");
-// let C_organisation = document.querySelector("#tbx2");
-// //choose identity
-// C_individual.addEventListener("click", () => {
-//     alert("button clicked");
-   
-// });
-
-// C_organisation.addEventListener("click", () => {
-//     alert("button clicked");
-    
-// });
-
-
+signupPagePopUP()
+chooseIdentity()
+profileDropdown()
